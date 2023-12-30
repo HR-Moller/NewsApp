eceabfeb91ff35625daef9ac66f8e28360b50601
Henrik Roos Møller <henrik.roos.moller@gmail.com>
2023-12-30 22:00:44 +0100

    Firebase Crashlytics and Analytics
    Added Firebase Dependencies for Crashlytics and analytics
    
    Added Button to force crash to test crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 allprojects {
@@ -94,4 +96,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-crashlytics")
 }
